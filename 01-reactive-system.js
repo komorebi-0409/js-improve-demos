f337951c724d53dfb6efdaa6198fba3df7438e4e
r2coder <r2coder@163.com>
2022-03-31 09:16:25 +0800

    addDependency、notify、watchEffect的实现

--- 01-reactive-system.js
+++ 01-reactive-system.js
@@ -1,3 +1,4 @@
+let currentDependency = null // 利用外部变量做到 addDependency 时不需要关心添加的依赖是什么，而由 watchEffect控制
 class DependenciesController {
   constructor() {
     this.dependencies = new Set()
@@ -6,13 +7,15 @@ class DependenciesController {
    * 收集依赖（每次收集一个函数，和watchEffect挂钩）
    */
   addDependency() {
-
+    if (currentDependency) {
+      this.dependencies.add(currentDependency)
+    }
   }
   /**
    * 触发依赖
    */
   notify() {
-
+    this.dependencies.forEach(fn => fn())
   }
 }
 
@@ -20,7 +23,9 @@ class DependenciesController {
 const dependenciesMap = new WeakMap() // 用于存放所有的依赖
 
 function watchEffect(fn){
-
+  currentDependency = fn
+  currentDependency()
+  currentDependency = null
 }
 
 function reactive(obj) {
