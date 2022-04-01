09d58a5d840159d183a5675928e7b054d0e2dad3
r2coder <r2coder@163.com>
2022-04-01 14:00:24 +0800

    添加注释

--- 02-promise.js
+++ 02-promise.js
@@ -5,11 +5,13 @@ function RPromise(executor) {
   var _this = this
 
   var resolve = function(result) {
+    // 如果状态已经改变 直接不添加任务
     if (_this.status !== RPromise.STATUS_PENDING) {
       return
     }
     // 对于支持的环境，可以使用queueMicrotask
     setTimeout(function() {
+      // 应在延时任务内改变状态，否则 then 方法返回的 promise 内永远都不是 pending 状态
       if (_this.status !== RPromise.STATUS_PENDING) {
         return
       }
@@ -24,7 +26,7 @@ function RPromise(executor) {
     if (_this.status !== RPromise.STATUS_PENDING) {
       return
     }
-    // 对于支持的环境，可以使用queueMicrotask
+    // 对于支持的环境，可以使用 queueMicrotask 代替 setTimeout
     setTimeout(function() {
       if (_this.status !== RPromise.STATUS_PENDING) {
         return
@@ -37,6 +39,7 @@ function RPromise(executor) {
     })
   }
 
+  // 执行 executor, 出错的话进入 rejected 状态
   try {
     executor(resolve, reject)
   } catch (err) {
@@ -45,6 +48,7 @@ function RPromise(executor) {
   
 }
 
+// 定义 promise 的三种状态
 Object.defineProperties(RPromise, {
   STATUS_FULFILLED: {
     configurable: false,
@@ -66,6 +70,13 @@ Object.defineProperties(RPromise, {
   }
 })
 
+/**
+ * 供 then 调用的方法：当 promise 状态改变时调用
+ * @param { function } fn 要执行的函数，应该是 onfulfilled 或 onrejected 中的一个
+ * @param { any } result 调用 resolve/reject 时传入的参数
+ * @param { resolve } resolve then 方法返回的 promise 的 resolve 对象
+ * @param { reject } reject then 方法返回的 promise 的 reject 对象
+ */
 function onRPromiseStatusChanged(fn, result, resolve, reject) {
   try {
     resolve(fn(result))
@@ -90,7 +101,9 @@ RPromise.prototype = {
       }
     }
     var _this = this
+    // 返回一个新的 promise，其状态调用者的回调函数执行状态决定：正常-fulfilled，异常-rejected
     return new RPromise(function(resolve, reject) {
+      // 调用 then 方法时，如果 promise 状态已经发生改变，直接调用回调函数
       if (_this.status === RPromise.STATUS_FULFILLED) {
         onRPromiseStatusChanged(onfulfilled, _this.value, resolve, reject)
         return
@@ -99,6 +112,8 @@ RPromise.prototype = {
         onRPromiseStatusChanged(onrejected, _this.value, resolve, reject)
         return
       }
+
+      // 保存回调函数，在 promise 状态发生改变时调用
       _this.onfulfilledFns.push(function(result) {
         onRPromiseStatusChanged(onfulfilled, result, resolve, reject)
       })
