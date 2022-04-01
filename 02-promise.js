d658bd2da2dd319c092ff71ec0e5a31abd47ca4c
r2coder <huxin1@szkingdom.com>
2022-04-01 22:18:23 +0800

    promise any、race方法

--- 02-promise.js
+++ 02-promise.js
@@ -3,8 +3,8 @@ function RPromise(executor) {
   this.onrejectedFns = []
   this.status = RPromise.STATUS_PENDING
   var _this = this
-  
-  function resolve (result) {
+
+  function resolve(result) {
     // result 是一个 promise 对象或 thenable 的处理
     if (typeof (result && result.then) === 'function') {
       result.then(resolve, reject)
@@ -16,14 +16,14 @@ function RPromise(executor) {
       return
     }
     // 对于支持的环境，可以使用queueMicrotask
-    setTimeout(function() {
+    setTimeout(function () {
       // 应在延时任务内改变状态，否则 then 方法返回的 promise 内永远都不是 pending 状态
       if (_this.status !== RPromise.STATUS_PENDING) {
         return
       }
       _this.status = RPromise.STATUS_FULFILLED
       _this.value = result
-      _this.onfulfilledFns.forEach(function(fn) {
+      _this.onfulfilledFns.forEach(function (fn) {
         fn(result)
       })
     })
@@ -33,7 +33,7 @@ function RPromise(executor) {
       return
     }
     // 对于支持的环境，可以使用 queueMicrotask 代替 setTimeout
-    setTimeout(function() {
+    setTimeout(function () {
       if (_this.status !== RPromise.STATUS_PENDING) {
         return
       }
@@ -43,7 +43,7 @@ function RPromise(executor) {
       if (!_this.onrejectedFns.length) {
         throw new Error('Uncaught (in promise) ' + reason)
       }
-      _this.onrejectedFns.forEach(function(fn) {
+      _this.onrejectedFns.forEach(function (fn) {
         fn(reason)
       })
     })
@@ -55,7 +55,7 @@ function RPromise(executor) {
   } catch (err) {
     reject(err)
   }
-  
+
 }
 
 // 定义 promise 的三种状态
@@ -99,20 +99,20 @@ function onRPromiseStatusChanged(fn, result, resolve, reject) {
  * 实例方法
  */
 RPromise.prototype = {
-  then: function(onfulfilled, onrejected) {
+  then: function (onfulfilled, onrejected) {
     if (typeof onfulfilled !== 'function') {
-      onfulfilled = function(res) {
+      onfulfilled = function (res) {
         return res
       }
     }
     if (typeof onrejected !== 'function') {
-      onrejected = function(err) {
+      onrejected = function (err) {
         throw err
       }
     }
     var _this = this
     // 返回一个新的 promise，其状态调用者的回调函数执行状态决定：正常-fulfilled，异常-rejected
-    return new RPromise(function(resolve, reject) {
+    return new RPromise(function (resolve, reject) {
       // 调用 then 方法时，如果 promise 状态已经发生改变，直接调用回调函数
       if (_this.status === RPromise.STATUS_FULFILLED) {
         onRPromiseStatusChanged(onfulfilled, _this.value, resolve, reject)
@@ -124,18 +124,18 @@ RPromise.prototype = {
       }
 
       // 保存回调函数，在 promise 状态发生改变时调用
-      _this.onfulfilledFns.push(function(result) {
+      _this.onfulfilledFns.push(function (result) {
         onRPromiseStatusChanged(onfulfilled, result, resolve, reject)
       })
-      _this.onrejectedFns.push(function(reason) {
+      _this.onrejectedFns.push(function (reason) {
         onRPromiseStatusChanged(onrejected, reason, resolve, reject)
       })
     })
   },
-  catch: function(onrejected) {
+  catch: function (onrejected) {
     return this.then(undefined, onrejected)
   },
-  finally: function(onStatusChanged) {
+  finally: function (onStatusChanged) {
     return this.then(onStatusChanged, onStatusChanged)
   }
 }
@@ -149,34 +149,32 @@ Object.defineProperty(RPromise.prototype, 'constructor', {
 
 // 静态方法
 var RPromiseStaticMethods = {
-  resolve: function(result) {
-    return new RPromise(function(resolve) {
+  resolve: function (result) {
+    return new RPromise(function (resolve) {
       resolve(result)
     })
   },
-  reject: function(reason) {
-    return new RPromise(function(resolve, reject) {
+  reject: function (reason) {
+    return new RPromise(function (resolve, reject) {
       reject(reason)
     })
   },
-  all: function(promises) {
-    return new RPromise(function(resolve, reject){
+  all: function (promises) {
+    return new RPromise(function (resolve, reject) {
       var values = []
       var _index = 0
-      promises.forEach(function(promise, index) {
+      promises.forEach(function (promise, index) {
         // 对普通对象的处理
         if (!(typeof (promise && promise.then) === 'function')) {
           promise = RPromise.resolve(promise)
         }
-        promise.then(function(res) {
+        promise.then(function (res) {
           values[index] = res
           _index++
           if (_index === promises.length) {
             resolve(values)
           }
-        }, function(reason) {
-          reject(reason)
-        })
+        }, reject)
       })
     })
   },
@@ -201,6 +199,31 @@ var RPromiseStaticMethods = {
             resolve(values)
           }
         })
+      })
+    })
+  },
+  any: function (promises) {
+    return new RPromise(function (resolve, reject) {
+      var _index = 0
+      var reasons = []
+      promises.forEach(function (promise, index) {
+        if (!(typeof (promise && promise.then) === 'function')) {
+          promise = RPromise.resolve(promise)
+        }
+        promise.then(resolve, function (reason) {
+          reasons[index] = reason
+          _index++
+          if (_index === promises.length) {
+            reject(new AggregateError(reasons, 'all promise rejected'))
+          }
+        })
+      })
+    })
+  },
+  race: function (promises) {
+    return new RPromise(function (resolve, reject) {
+      promises.forEach(function (promise) {
+        promise.then(resolve, reject)
       })
     })
   }
